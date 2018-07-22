754bca354285315afeec963caee93ef0dce65bb1
周鑫 <15901263220@163.com>
2018-07-22 17:05:28 +0800

    初始化

---
+++ lalllalalll/lalllalalll/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  lalllalalll
+//
+//  Created by 周鑫 on 2018/7/22.
+//  Copyright © 2018年 ZX. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
